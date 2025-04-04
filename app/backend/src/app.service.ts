2262321d362cb8e05fdf8205ed04c9838a6988ef
huu thiet <huuthietn01@gmail.com>
2025-04-04 13:53:58 +0700

    test commit

--- app/backend/src/app.service.ts
+++ app/backend/src/app.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     console.log('Hello World!');
-    return 'Hello World!';
+    return 'Hello World!-hihi';
   }
 }
